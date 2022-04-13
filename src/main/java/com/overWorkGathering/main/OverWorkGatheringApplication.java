2dc4c61299e24d16992ea9bb8920d8026d6d59e4
junhop <hirofac@naver.com>
2022-04-13 22:03:34 +0900

    프로젝트 시작!!!

---
+++ src/main/java/com/overWorkGathering/main/OverWorkGatheringApplication.java
@@ -0,0 +1,13 @@
+package com.overWorkGathering.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OverWorkGatheringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OverWorkGatheringApplication.class, args);
+	}
+
+}
