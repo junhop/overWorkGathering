0e7f268b094c30a3c69510845703674f6a332a7f
jhyuk <jhyuk@DESKTOP-HCJELPQ>
2022-05-28 20:49:26 +0900

    5월28일 최재혁 templates 폴터생서

---
+++ src/main/java/com/overWorkGathering/main/controller/CalendarController.java
@@ -0,0 +1,16 @@
+package com.overWorkGathering.main.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class CalendarController {
+	
+	@RequestMapping(value="/calendar")
+	public String calendar() {
+		System.out.println("CalendarCalendarCalendarCalendarCalendar");
+		
+		return "Calendar";
+	}
+}
